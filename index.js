d84f2541b364057ce9f90813d0ff995a9af9da46
Juthapong <s6206021621091@email.kmutnb.ac.th>
2023-08-14 20:37:12 +0700

    create

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 // const port =3000;
-const port = process.env.PORT;
+const port = process.env.PORT||3000;
 
 app.get("/", (req, res) => {
-  res.send("hello world!");
+  res.send("hello world! Hello");
 });
 
 app.listen(port, () => {
